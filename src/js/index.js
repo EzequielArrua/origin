8c21925db11951b0c18ba090e5364e996c63c38a
EzequielArrua <ezemaccrak@outlook.es>
2024-08-23 21:25:07 -0300

    tercer commit

--- src/js/index.js
+++ src/js/index.js
@@ -16,22 +16,35 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 const btn = document.getElementById('boton-correo');
+const confirmacion = document.getElementById('confirmacion')
 
 document.getElementById('contacto-form')
  .addEventListener('submit', function(event) {
    event.preventDefault();
 
-   btn.value = 'Sending...';
+   btn.value = 'Enviando email...';
 
    const serviceID = 'default_service';
    const templateID = 'template_q65kj18';
 
    emailjs.sendForm(serviceID, templateID, this)
     .then(() => {
-      btn.value = 'Send Email';
-      alert('Sent!');
+      btn.value = 'Email enviado';
+      confirmacion.innerHTML = ''
+      confirmacion.innerHTML = `<div class="alert alert-success alert-dismissible fade show mt-3" role="alert">
+      <strong>Mail enviado exitosamente!</strong>
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>`
+      document.getElementById('emailjs_message').value = ""
+      document.getElementById('emailjs_email').value = ""
+      document.getElementById('emailjs_name').value = ""
     }, (err) => {
       btn.value = 'Send Email';
+      confirmacion.innerHTML = ''
+      confirmacion.innerHTML = `<div class="alert alert-danger alert-dismissible fade show mt-3" role="alert">
+      <strong>Error al enviar el mail!</strong>
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>`
       alert(JSON.stringify(err));
     });
 });
